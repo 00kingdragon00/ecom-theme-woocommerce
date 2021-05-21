0c36be26408620a86799516217c1a5bacd384639
king-dragon <utkarshchauhan17@outlook.com>
2021-05-21 09:30:10 +0530

    create announcement bar

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,77 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'ecom' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'ecom'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$ecom_description = get_bloginfo( 'description', 'display' );
-			if ( $ecom_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $ecom_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="announcement-bar pt-2 pb-2 ">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<ul class="announcement-bar__list">
+							<li>
+								<i class="bi bi-telephone rounded-circle"></i>
+								<a href="tel:+91-123-456565">+91-123-456565</a>
+							</li>
+							<li>
+								<i class="bi bi-envelope rounded-circle"></i>
+								<a href="mail:hello@ecom.local">hello@ecom.local</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-8 d-flex justify-content-end">
+						<ul class="announcement-bar__list">
+							<li>
+								<i class="bi bi-truck rounded-circle"></i>
+								Free Shipping
+							</li>
+							<li>
+								<i class="bi bi-clock-history rounded-circle"></i>
+								30 DAY MONEYBACK GUARANTEE
+							</li>
+							<li>
+								<i class="bi bi-person rounded-circle"></i>
+								24/7 CUSTOMER SUPPORT
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ecom' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				if (is_front_page() && is_home()) :
+				?>
+					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+				<?php
+				else :
+				?>
+					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+				<?php
+				endif;
+				$ecom_description = get_bloginfo('description', 'display');
+				if ($ecom_description || is_customize_preview()) :
+				?>
+					<p class="site-description">
+						<?php echo $ecom_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+						?>
+					</p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'ecom'); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
