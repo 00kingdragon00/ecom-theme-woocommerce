52178542bb816881396dc29913ef55cc889af577
king-dragon <utkarshchauhan17@outlook.com>
2021-05-21 12:03:25 +0530

    create header ui

--- header.php
+++ header.php
@@ -15,85 +15,101 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'ecom'); ?></a>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'ecom'); ?></a>
 
-		<div class="announcement-bar pt-2 pb-2 ">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<ul class="announcement-bar__list">
-							<li>
-								<i class="bi bi-telephone rounded-circle"></i>
-								<a href="tel:+91-123-456565">+91-123-456565</a>
-							</li>
-							<li>
-								<i class="bi bi-envelope rounded-circle"></i>
-								<a href="mail:hello@ecom.local">hello@ecom.local</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-md-8 d-flex justify-content-end">
-						<ul class="announcement-bar__list">
-							<li>
-								<i class="bi bi-truck rounded-circle"></i>
-								Free Shipping
-							</li>
-							<li>
-								<i class="bi bi-clock-history rounded-circle"></i>
-								30 DAY MONEYBACK GUARANTEE
-							</li>
-							<li>
-								<i class="bi bi-person rounded-circle"></i>
-								24/7 CUSTOMER SUPPORT
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
+        <div class="announcement-bar pt-2 pb-2 ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <ul class="announcement-bar__list">
+                            <li>
+                                <i class="bi bi-telephone rounded-circle"></i>
+                                <a class="text-decoration-none" href="tel:+91-123-456565">+91-123-456565</a>
+                            </li>
+                            <li>
+                                <i class="bi bi-envelope rounded-circle"></i>
+                                <a class="text-decoration-none" href="mail:hello@ecom.local">hello@ecom.local</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-8 d-flex justify-content-end">
+                        <ul class="announcement-bar__list">
+                            <li>
+                                <i class="bi bi-truck rounded-circle"></i>
+                                Free Shipping
+                            </li>
+                            <li>
+                                <i class="bi bi-clock-history rounded-circle"></i>
+                                30 DAY MONEYBACK GUARANTEE
+                            </li>
+                            <li>
+                                <i class="bi bi-person rounded-circle"></i>
+                                24/7 CUSTOMER SUPPORT
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if (is_front_page() && is_home()) :
-				?>
-					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-				<?php
-				else :
-				?>
-					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-				<?php
-				endif;
-				$ecom_description = get_bloginfo('description', 'display');
-				if ($ecom_description || is_customize_preview()) :
-				?>
-					<p class="site-description">
-						<?php echo $ecom_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-						?>
-					</p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
+        <header id="masthead" class="site-header">
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'ecom'); ?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
+            <div class="container pt-2 pb-2">
+                <div class="row align-items-center">
+                    <div class="col site-header__logo d-flex justify-content-center justify-content-md-start pb-2">
+                        <?php the_custom_logo(); ?>
+                    </div>
+                    <div class="col-md-5"><?php if (function_exists('aws_get_search_form')) {
+												aws_get_search_form();
+											} ?></div>
+                    <div class="col cart d-flex justify-content-center justify-content-md-end align-items-center pt-2">
+                        <a href="<?php echo wc_get_cart_url(); ?>">
+                            <i class="bi bi-bag-dash p-2"></i>
+                        </a>
+                        <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>"
+                            title="<?php _e('View your shopping cart'); ?>">
+                            <?php echo sprintf(_n('%d item', '%d items', WC()->cart->get_cart_contents_count()), WC()->cart->get_cart_contents_count()); ?>
+                            - <?php echo WC()->cart->get_cart_total(); ?></a>
+                    </div>
+                </div>
+            </div><!-- .site-branding -->
+
+            <nav id="site-navigation" class="main-navigation bg-primary">
+
+                <div class="container d-flex justify-content-center">
+
+                    <div class="row">
+                        <div class="col-12 d-flex justify-content-center">
+                            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                                <i class="bi bi-list"></i>
+                                <?php esc_html_e('Primary Menu', 'ecom'); ?></button>
+                        </div>
+                        <div class="col-12 tex-center">
+                            <?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-1',
+									'menu_id'        => 'primary-menu',
+								)
+							);
+							?>
+
+                        </div>
+                    </div>
+                </div>
+
+            </nav><!-- #site-navigation -->
+
+
+
+        </header><!-- #masthead -->
